8d17ef8047c64e6d0b57a78fe8a95bb48ef38859
Ritesh230087 <riteshdaas01@gmail.com>
2026-03-30 15:40:36 +0545

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import fs from 'fs'
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+
+//     // host: '0.0.0.0',          //to run in kali
+
+// https: {
+//   key: fs.readFileSync('./localhost+2-key.pem'),
+//   cert: fs.readFileSync('./localhost+2.pem'),
+// },
+//     port: 5173,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//         secure: false
+//       }
+//     }
+//   }
+// })
+
+
+
+import { defineConfig } from "vite";
+import fs from "fs";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-https: {
-  key: fs.readFileSync('./localhost+2-key.pem'),
-  cert: fs.readFileSync('./localhost+2.pem'),
-},
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
-
+  server:
+    process.env.NODE_ENV === "development"
+      ? {
+          https: {
+            key: fs.readFileSync("./localhost+2-key.pem"),
+            cert: fs.readFileSync("./localhost+2.pem"),
+          },
+        }
+      : {},
+});
